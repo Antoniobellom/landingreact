930dfe1339c9dbbaf65f27ced01453fef09ce057
Antoniobellom <antoniobellom14@gmail.com>
2020-04-04 18:12:39 -0300

    components con errorers

--- src/js/index.js
+++ src/js/index.js
@@ -9,7 +9,13 @@ import "bootstrap";
 import "../styles/index.scss";
 
 //import your own components
-import { Home } from "./component/home.js";
+import { Home, Andres } from "./component/home.js";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+ReactDOM.render(
+	<div>
+		<Home />
+		<Andres />
+	</div>,
+	document.querySelector("#app")
+);
