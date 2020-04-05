8daddf92820f13ae49327ea598aa84a6e389eb4c
patodekaz <pato@dekaz.cl>
2020-04-05 18:23:49 -0400

    arreglo footer

--- src/js/component/Footer.js
+++ src/js/component/Footer.js
@@ -2,11 +2,14 @@ import React from "react";
 
 function Footer() {
 	return (
-		<footer className="page-footer font-small blue">
+		<footer className="page-footer font-small text-light bg-dark py-4">
 			<div className="container">
 				<div className="footer-copyright text-center py-3">
 					© 2020 Copyright:
-					<a href="#"> Este sitio sin Coronavirus 2020</a>
+					<a href="#" className="text-light">
+						{" "}
+						Este sitio sin Coronavirus 2020
+					</a>
 				</div>
 			</div>
 		</footer>
